8a4391b5642e9011438d84302371a5a063c66347
Sean Thomas <Sean.M.Thomas.0@gmail.com>
2016-10-13 11:24:18 -0400

    add basic solution to problem 3

---
+++ Problem3/Euler3.java
@@ -0,0 +1,30 @@
+public class Euler3 {
+    public static long calculatelargestPrimeFactor(long value){
+        if( value < 1) {
+            return 0;
+        }
+        long remainingValue = value;
+        long testFactor = 2;
+        long sqrtChecker = testFactor * testFactor;
+        while( sqrtChecker < remainingValue) {
+
+            if( (remainingValue % testFactor) == 0) {
+                // testFactor is a factor of remainingValue
+                remainingValue /= testFactor;
+                // reset testFactor for this value in case it 
+                // appears multiple times in the factorization
+                testFactor--;
+            }
+            testFactor++;
+            sqrtChecker = testFactor * testFactor;
+        }
+        // remainingValue should hold the larges Prime Factor 
+        return remainingValue;
+    }
+    public static void main(String args[]){
+        //Scanner reader = new Scanner(System.in);
+        //System.out.print("Enter a number: ");
+        //long n = reader.nextLong();
+        System.out.println(calculatelargestPrimeFactor(600851475143L));
+    }
+}
